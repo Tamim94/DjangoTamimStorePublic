fd21983f4656b6baa2c692cd62c91006bbac1927
tamim94 <golam.tamim94@gmail.com>
2024-06-08 16:58:41 +0200

    commit

--- djangoTamimStore/settings.py
+++ djangoTamimStore/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_root'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type

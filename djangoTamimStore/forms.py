4b3438556eba8426c46e7176bdea278ef095539d
tamim94 <golam.tamim94@gmail.com>
2024-06-08 19:05:53 +0200

    commit

--- djangoTamimStore/forms.py
+++ djangoTamimStore/forms.py
@@ -9,6 +9,9 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Post
         fields = ['title', 'content']
